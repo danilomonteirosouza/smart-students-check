bcf9942780d0286362a47a601af1e4ff78636fe0
Danilo Monteiro Souza <danilomonteirosouza1@gmail.com>
2023-03-20 14:57:12 -0300

    SplashScreen was created

---
+++ android/app/src/main/kotlin/awaken/com/smart_students_check/MainActivity.kt
@@ -0,0 +1,6 @@
+package awaken.com.smart_students_check
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:smart_students_check/views/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    const MaterialApp(
+      title: "Smart Student's Check",
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
