62e96b82bfc1c99fb5c55b3a3ef83f5f02c0a116
Luiz Gabriel Pacheco <luizgabrielpacheco.1006@gmail.com>
2023-03-27 15:24:02 -0300

    Criação da tela de cadastro

--- lib/views/register.dart
+++ lib/views/register.dart
@@ -10,47 +10,92 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  String nome = '';
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    if(defaultTargetPlatform == TargetPlatform.android){
-      return Scaffold(
-          backgroundColor: const Color(0xff00ceff),
-          appBar: AppBar(
-            title: const Text('In progress ...'),
-            backgroundColor: const Color(0xff00ceff),
-            elevation: 0.0,
-            centerTitle: true,
-          ),
-          body: Container(
-              color: const Color(0xff00ceff),
-              child: Stack(
-                children: <Widget>[
-                  Column(
-                    children: const <Widget>[
-                      Expanded(
-                          flex: 1,
-                          child: Padding(
-                            padding: EdgeInsets.zero,
-                            child: Center(
-                              child: Text('In progress ...'),
-                            ),
-                          )
+    //Criação do titulo cadastro na AppBar
+    return Scaffold(
+      backgroundColor: const Color(0xff00ceff),
+      appBar: AppBar(
+        title: const Text('Cadastro'),
+        backgroundColor: const Color(0xff00ceff),
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      body: Container(
+        //Preenchendo o corpo da tela com a cor padrão
+        color: const Color(0xff00ceff),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  //Widget coluna usado junto com o TextField para colocar as strings recebidas do usuario
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          nome = text;
+                        });
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Nome",
+                        border: OutlineInputBorder(),
                       ),
-                    ],
-                  ),
-                ],
-              )
-          )
-      );
-    } else if(defaultTargetPlatform == TargetPlatform.iOS){
-      return const CupertinoPageScaffold(
-          navigationBar: CupertinoNavigationBar(
-            middle: Text('In progress ...'),
-          ),
-          child: Text('In Progress')
-      );
-    } else {
-      return const Text('System not detected');
-    }
+                    ),
+                    const SizedBox(height: 15),
+                    TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          email = text;
+                        });
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          password = text;
+                        });
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (email == "luizgabriel@desenvolvedor.com.br" &&
+                            password == "123") {
+                          print("Login correto");
+                        } else {
+                          print("Login inválido");
+                        }
+                      },
+                      child: const Text("Entrar"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
