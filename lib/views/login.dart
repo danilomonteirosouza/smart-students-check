169ed9c103e7af9227320d63961e759bc3b114b5
Luiz Gabriel Pacheco <luizgabrielpacheco.1006@gmail.com>
2023-03-28 15:09:16 -0300

    Modificação na tela registro alterando a cor de fundo e implementação do copyright e começo da contrução da tela de login

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -14,40 +14,58 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     if(defaultTargetPlatform == TargetPlatform.android){
       return Scaffold(
+        backgroundColor: const Color(0xff00ceff),
+        appBar: AppBar(
+          title: const Text('Login'),
           backgroundColor: const Color(0xff00ceff),
-          appBar: AppBar(
-            title: const Text('In progress.'),
-            backgroundColor: const Color(0xff00ceff),
-            elevation: 0.0,
-            centerTitle: true,
-          ),
-          body: Container(
-              color: const Color(0xff00ceff),
-              child: Stack(
-                children: <Widget>[
-                  Column(
-                    children: const <Widget>[
-                      Expanded(
-                          flex: 1,
-                          child: Padding(
-                            padding: EdgeInsets.zero,
-                            child: Center(
-                              child: Text('In progress ...'),
-                            ),
-                          )
-                      ),
-                    ],
-                  ),
+          elevation: 0.0,
+          centerTitle: true,
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xff00ceff),
+                  Colors.white38,
                 ],
               )
-          )
+          ),
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder()
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Senha",
+                      border: OutlineInputBorder()
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+        ),
       );
     } else if(defaultTargetPlatform == TargetPlatform.iOS){
       return const CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: Text('In progress ...'),
-        ),
-        child: Text('In Progress')
+          navigationBar: CupertinoNavigationBar(
+            middle: Text('In progress ...'),
+          ),
+          child: Text('In Progress')
       );
     } else {
       return const Text('System not detected');
--- lib/views/register.dart
+++ lib/views/register.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
@@ -36,7 +37,16 @@ class _RegisterState extends State<Register> {
       ),
       body: Container(
         //Preenchendo o corpo da tela com a cor padrão
-        color: const Color(0xff00ceff),
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xff00ceff),
+                Colors.white38,
+              ],
+            )
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -118,6 +128,130 @@ class _RegisterState extends State<Register> {
                   ],
                 )
             ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+ */
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import '../routes/routegenerator.dart';
+import '../reusable_widgets/elevatedbuttonlong.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  String nome = '';
+  String email = '';
+  String password = '';
+
+  void initialRoute(){
+    Navigator.pushNamed(
+        context,
+        RouteGenerator.ROTA_INICIAL
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //Criação do titulo cadastro na AppBar
+    return Scaffold(
+      backgroundColor: const Color(0xff00ceff),
+      appBar: AppBar(
+        title: const Text('Cadastro'),
+        backgroundColor: const Color(0xff00ceff),
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      body: Container(
+        //Preenchendo o corpo da tela com a cor padrão
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xff00ceff),
+                Colors.white38,
+              ],
+            )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  //Widget coluna usado junto com o TextField para colocar as strings recebidas do usuario
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          nome = text;
+                        });
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Nome",
+                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          email = text;
+                        });
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          password = text;
+                        });
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    BotaoCustomizadoLong(
+                      texto: "Entrar" ,
+                      onPressed: (){
+                        initialRoute();
+                      },
+                    )
+                  ],
+                ),
+              ),
             ),
           ],
         ),
